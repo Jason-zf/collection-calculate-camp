eef52500b365af715f9694326f5f2d0e08f1161d
bfzheng <baofeng_zheng@163.com>
2018-09-03 23:31:30 +0800

    [superposition_operation] finish collection operation letter mapping 2

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function average_to_letter(collection) {
-
+  var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
   //在这里写入代码
+  return letters[Math.floor(collection.reduce((a, b) => a + b) / collection.length)];
 }
 
 module.exports = average_to_letter;
