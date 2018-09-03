4330459c6a57ec5cd6d28069d928ac06187d02d4
bfzheng <baofeng_zheng@163.com>
2018-09-03 21:38:30 +0800

    [reduce] finish find last even

--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -2,6 +2,8 @@
 
 function find_last_even(collection) {
   //在这里写入代码
+  var evens = collection.filter(number => number % 2 === 0);
+  return evens[evens.length - 1];
 }
 
 module.exports = find_last_even;
