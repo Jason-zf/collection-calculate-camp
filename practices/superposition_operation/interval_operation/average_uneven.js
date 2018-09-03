bc73d403cbfcbdc10e9d58987d4914e5db4157ff
bfzheng <baofeng_zheng@163.com>
2018-09-03 23:41:38 +0800

    [superposition_operation] finish interval operation average uneven

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -3,6 +3,8 @@
 function average_uneven(collection) {
 
   //在这里写入代码
+  var odds = collection.filter(value => value % 2 === 1);
+  return odds.reduce((a, b) => a + b) / odds.length;
 }
 
 module.exports = average_uneven;
