830dca681aae5c7638f47345a67ff38edaf4b6fc
bfzheng <baofeng_zheng@163.com>
2018-09-03 22:14:39 +0800

    [section_1] finish practice 4

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,10 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var keys = [];
+  for (var i = 0; i < collection_a.length; i++) {
+    keys.push(collection_a[i].key);
+  }
+  return keys.filter(value => object_b.value.includes(value));
 }
 
 module.exports = collect_same_elements;
