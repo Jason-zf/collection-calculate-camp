6c412fc2abcd3420eea7cb1244392e9b53135d7d
bfzheng <baofeng_zheng@163.com>
2018-09-03 21:35:42 +0800

    [reduce] finish find element last subscript

--- practices/reduce/find_element_last_subscript.js
+++ practices/reduce/find_element_last_subscript.js
@@ -2,6 +2,13 @@
 
 function calculate_elements_sum(collection, element) {
   //在这里写入代码
+  var indexs = [];
+  var idx = collection.indexOf(element);
+  while (idx != -1) {
+    indexs.push(idx);
+    idx = collection.indexOf(element, idx + 1);
+  }
+  return indexs[indexs.length - 1];
 }
 
 module.exports = calculate_elements_sum;
