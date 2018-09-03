0d0d3e1e8fbba121523f79c630888d238719ce19
bfzheng <baofeng_zheng@163.com>
2018-09-03 23:50:21 +0800

    [superposition_operation] finish interval operation median to letter

--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -3,6 +3,10 @@
 function median_to_letter(collection) {
 
   //在这里写入代码
+  let lowMiddle = Math.floor((collection.length - 1) / 2);
+  let highMiddle = Math.ceil((collection.length - 1) / 2);
+  let median = Math.round((collection[lowMiddle] + collection[highMiddle]) / 2);
+  return median === 37 ? 'ak' : "";
 }
 
 module.exports = median_to_letter;
