601a9eefe98f11efbd06db091989c23415169d56
bfzheng <baofeng_zheng@163.com>
2018-09-03 21:17:04 +0800

    [reduce] finish compute median

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,6 +2,7 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
+
 }
 
 module.exports = compute_chain_median;
--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -2,6 +2,10 @@
 
 function compute_median(collection) {
   //在这里写入代码
+  let lowMiddle = Math.floor((collection.length - 1) / 2);
+  let highMiddle = Math.ceil((collection.length - 1) / 2);
+  let median = (collection[lowMiddle] + collection[highMiddle]) / 2;
+  return median;
 }
 
 module.exports = compute_median;
