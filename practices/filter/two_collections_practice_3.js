b33bf5c50045e6beff11e941b585ac094a14e77e
bfzheng <baofeng_zheng@163.com>
2018-09-03 17:40:02 +0800

    [filter] finish two collection practice 3

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -3,6 +3,7 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //在这里写入代码
+  return collection_a.filter(number_a => collection_b.filter(number_b => number_a % number_b === 0).length > 0);
 }
 
 module.exports = choose_divisible_integer;
