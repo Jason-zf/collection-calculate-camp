9f04f1e576b7f0f3a6263cb2cde710491a443e36
bfzheng <baofeng_zheng@163.com>
2018-09-03 19:28:28 +0800

    [map] finish number map to word over 26

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,7 @@
 'use strict';
-var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+var number_map_to_word_over_26 = function (collection) {
+  var strings = ['a', 'm', 'aa', 'ad', 'y', 'aa'];
+  return collection.map(number => strings[collection.indexOf(number)]);
 };
 
 module.exports = number_map_to_word_over_26;
