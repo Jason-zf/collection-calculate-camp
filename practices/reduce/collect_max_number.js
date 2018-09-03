04492afdd666bc1f8c1c9656c2230777d6aad3c7
bfzheng <baofeng_zheng@163.com>
2018-09-03 19:37:25 +0800

    [reduce] finish collect max number

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -2,6 +2,8 @@
 
 function collect_max_number(collection) {
   //在这里写入代码
+  const comparator = (a, b) => a > b ? a : b;
+  return collection.reduce(comparator);
 }
 
 module.exports = collect_max_number;
