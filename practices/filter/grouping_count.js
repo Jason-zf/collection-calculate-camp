bbc34b5b9e5b82241e145df3279364aa9f9634b0
bfzheng <baofeng_zheng@163.com>
2018-09-03 17:28:16 +0800

    [filter] finish grouping count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,15 @@
 function grouping_count(collection) {
 
   //在这里写入代码
+  const selector = (arr, val) => {
+    if (val in arr) {
+      arr[val]++;
+    } else {
+      arr[val] = 1;
+    }
+    return arr;
+  }
+  return collection.reduce(selector, {});
 }
 
 module.exports = grouping_count;
