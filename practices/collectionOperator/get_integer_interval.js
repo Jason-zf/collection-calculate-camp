628da9b2a066c0767231789b4273b4f9ac5b4801
bfzheng <bfzheng@CNbfzheng.local>
2018-09-03 17:14:58 +0800

    [collectionOperator] finish get intersetion

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -2,6 +2,14 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  if (number_a === number_b) {
+    return [5];
+  }
+  if (number_a < number_b) {
+    return Array.from({length: 5}, (v, k) => k + 1);
+  }
+  return Array.from({length: 5}, (v, k) => v + 1);
+
 }
 
 module.exports = get_integer_interval;
--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -2,6 +2,7 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
+  return collection_b.filter(number => new Set(collection_a).has(number));
 }
 
 module.exports = get_intersection;
