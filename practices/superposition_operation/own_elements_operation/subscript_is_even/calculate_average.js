35a36b0cd20c8a6f26625ce3bacd9f9ae7e90566
bfzheng <baofeng_zheng@163.com>
2018-09-03 23:55:55 +0800

    [superposition_operation] finish own elements operation subscript is even calculate average

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,6 @@
 'use strict';
-var calculate_average = function(collection){
-
+var calculate_average = function (collection) {
+  var arry = collection.filter(value => (collection.indexOf(value) + 1) % 2 === 0);
+  return arry.reduce((a, b) => a + b) / arry.length;
 };
 module.exports = calculate_average;
