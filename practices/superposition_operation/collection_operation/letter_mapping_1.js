d8ed80010983142513fb4dca3b417b484e284e31
bfzheng <baofeng_zheng@163.com>
2018-09-03 23:28:07 +0800

    [superposition_operation] finish collection operation letter mapping 1

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function even_to_letter(collection) {
+  var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
 
   //在这里写入代码
+  return collection.filter(value => value % 2 === 0).map(value => letters[collection.indexOf(value)]);
 }
 
 module.exports = even_to_letter;
