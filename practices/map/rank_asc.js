0c827eb493c6fb59dbc0fa327fe5300d639fcbb1
bfzheng <baofeng_zheng@163.com>
2018-09-03 19:33:53 +0800

    [map] finish rank asc

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,6 +1,7 @@
 'use strict';
-var rank_asc = function(collection){
-  return [6,5,4,3,2];
+var rank_asc = function (collection) {
+  const comparator = (a, b) => a < b ? 1 : -1;
+  return collection.sort(comparator);
 };
 
 module.exports = rank_asc;
