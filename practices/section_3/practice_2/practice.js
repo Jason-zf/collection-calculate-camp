7272e96c4096deff470ca4c4138b256b664c15c9
bfzheng <baofeng_zheng@163.com>
2018-09-03 23:12:58 +0800

    [section_3] finish practice 2

--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -1,5 +1,11 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  for (var i = 0; i < collection_a.length; i++) {
+    if (object_b.value.includes(collection_a[i].key)) {
+      collection_a[i].count -= Math.floor(collection_a[i].count / 3);
+    }
+  }
+  return collection_a;
 }
 
 module.exports = create_updated_collection;
