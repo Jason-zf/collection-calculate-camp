24661f72fdd90773b45ed947f257c90ae6120c35
bfzheng <baofeng_zheng@163.com>
2018-09-03 23:35:05 +0800

    [superposition_operation] finish collection operation practice 2

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -3,6 +3,7 @@
 function hybrid_operation_to_uneven(collection) {
 
   //在这里写入代码
+  return collection.filter(value => value % 2 === 1).map(value => value * 3 + 2);
 }
 
 module.exports = hybrid_operation_to_uneven;
