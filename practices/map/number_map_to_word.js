5f90ab2f17aafe80e2ff0ef0330f7c45c081cae9
bfzheng <baofeng_zheng@163.com>
2018-09-03 19:25:48 +0800

    [map] finish number map to word

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,7 @@
 'use strict';
-var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+var number_map_to_word = function (collection) {
+  var strings = ['a', 'b', 'c', 'd', 'e'];
+  return collection.map(number => strings[collection.indexOf(number)]);
 };
 
 module.exports = number_map_to_word;
