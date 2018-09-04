42735411bbd6a099f2006f34ebf96271ce1f08c3
bfzheng <baofeng_zheng@163.com>
2018-09-04 16:29:01 +0800

    [collectionOperator] modify get integer interval

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -2,14 +2,8 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
-  if (number_a === number_b) {
-    return [5];
-  }
-  if (number_a < number_b) {
-    return Array.from({length: 5}, (v, k) => k + 1);
-  }
-  return Array.from({length: 5}, (v, k) => v + 1);
-
+  var array = [...Array(Math.abs(number_a - number_b) + 1).keys()].map(value => value + Math.min(number_a, number_b));
+  return number_a < number_b ? array : array.reverse();
 }
 
 module.exports = get_integer_interval;
