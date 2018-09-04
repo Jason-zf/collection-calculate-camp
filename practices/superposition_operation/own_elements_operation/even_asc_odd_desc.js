e2fe60f380156f70c0bcbbb77c4cbc1b3c0c2641
bfzheng <baofeng_zheng@163.com>
2018-09-04 11:14:25 +0800

    [superposition operation] finish own elements operation even asc odd desc

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,7 @@
 'use strict';
-var even_asc_odd_desc = function(collection){
-
+var even_asc_odd_desc = function (collection) {
+  var odds = collection.filter(value => value % 2 === 1);
+  var evens = collection.filter(value => value % 2 === 0);
+  return evens.sort((a, b) => a > b ? 1 : -1).concat(odds.sort((a, b) => a < b ? 1 : -1));
 };
 module.exports = even_asc_odd_desc;
