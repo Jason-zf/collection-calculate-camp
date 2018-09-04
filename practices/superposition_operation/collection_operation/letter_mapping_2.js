469577cfe66f72858f3230a4dccc10e2fe8f57ce
bfzheng <baofeng_zheng@163.com>
2018-09-04 10:02:15 +0800

    [superposition operation] modify collection operation letter mapping 2

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -3,7 +3,12 @@
 function average_to_letter(collection) {
   var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
   //在这里写入代码
-  return letters[Math.floor(collection.reduce((a, b) => a + b) / collection.length)];
+  const mapper = (value) => {
+    var numOfA = value / 26;
+    var charCode = value % 26 + 'a'.charCodeAt(0) - 1;
+    return 'a'.repeat(numOfA) + String.fromCharCode(charCode);
+  }
+  return mapper(Math.round(collection.reduce((a, b) => a + b) / collection.length));
 }
 
 module.exports = average_to_letter;
