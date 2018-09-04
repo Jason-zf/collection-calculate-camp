f6a10e48ff1dc6531351c325686855129b08958b
bfzheng <baofeng_zheng@163.com>
2018-09-04 17:57:00 +0800

    [reduce] finish compute chain median

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,9 +2,11 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
-  for (var obj in collection) {
-    // console.log(obj);
-  }
+  var array = collection.split('->').map(value => parseInt(value)).sort((a, b) => a < b ? 1 : -1);
+  var lowMiddle = Math.floor((array.length - 1) / 2);
+  var highMiddle = Math.ceil((array.length - 1) / 2);
+  var median = (array[lowMiddle] + array[highMiddle]) / 2;
+  return median;
 }
 
 module.exports = compute_chain_median;
