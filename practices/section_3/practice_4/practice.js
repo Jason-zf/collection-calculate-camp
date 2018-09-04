9b22211b8177299552cb42e9d9b2253834bfa9eb
bfzheng <baofeng_zheng@163.com>
2018-09-04 21:05:16 +0800

    [section 3] finish practice 4

--- practices/section_3/practice_4/practice.js
+++ practices/section_3/practice_4/practice.js
@@ -1,5 +1,27 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  const selector = (arr, val) => {
+    var c = Number.isNaN(parseInt(val.replace(/[^0-9]/g, ''))) === true ? 1 : parseInt(val.replace(/[^0-9]/g, ''));
+    var k = val.match(/^[A-Za-z]*/g)[0];
+    var flag = false;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i].key === k) {
+        arr[i].count += c;
+        flag = true;
+      }
+    }
+    if (flag === false) {
+      arr.push({'key': k, 'count': c});
+    }
+    return arr;
+  }
+  var array = collection_a.reduce(selector, []);
+  for (var i = 0; i < array.length; i++) {
+    if (object_b.value.includes(array[i].key)) {
+      array[i].count -= Math.floor(array[i].count / 3);
+    }
+  }
+  return array;
 }
 
 module.exports = create_updated_collection;
