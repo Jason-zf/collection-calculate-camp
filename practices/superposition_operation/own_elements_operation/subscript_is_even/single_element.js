e48041a78b01bc5231a54d25c8c520f75c7fd0b7
bfzheng <baofeng_zheng@163.com>
2018-09-04 11:02:21 +0800

    [superposition operation] finish own elements operation subscript is even single element

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,11 @@
 'use strict';
-var single_element = function(collection){
-
+var single_element = function (collection) {
+  var array = [];
+  for (var i = 0; i < collection.length; i++) {
+    if ((i + 1) % 2 === 0) {
+      array.push(collection[i]);
+    }
+  }
+  return array.filter(value => array.indexOf(value, array.indexOf(value) === undefined));
 };
 module.exports = single_element;
