216afe25116413805b5118fb2552c076c897e4dd
bfzheng <baofeng_zheng@163.com>
2018-09-04 15:45:40 +0800

    [superposition operation] modify even group calculate average

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -17,3 +17,4 @@ function rank_by_two_large_one_small(collection) {
 }
 
 module.exports = rank_by_two_large_one_small;
+
--- practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,10 +1,13 @@
 'use strict';
 var even_group_calculate_average = function (collection) {
-  var arry = collection.filter(value => (collection.indexOf(value) + 1) % 2 === 0).filter(number => number % 2 === 0);
+  var array = collection.filter(value => (collection.indexOf(value) + 1) % 2 === 0).filter(number => number % 2 === 0);
+  if (array.length === 0) {
+    return [0];
+  }
   var res = [];
   const sum = (a, b) => (a + b);
-  for (var i = 0; Math.pow(10, i) < arry.sort()[arry.length - 1]; i++) {
-    var temp = arry.filter(number => (number > Math.pow(10, i) - 1 && number < Math.pow(10, i + 1)));
+  for (var i = 0; Math.pow(10, i) < array.sort()[array.length - 1]; i++) {
+    var temp = array.filter(number => (number > Math.pow(10, i) - 1 && number < Math.pow(10, i + 1)));
     if (temp.length > 0) {
       res.push(temp.reduce(sum) / temp.length);
     }
@@ -12,3 +15,5 @@ var even_group_calculate_average = function (collection) {
   return res;
 }
 module.exports = even_group_calculate_average;
+
+
