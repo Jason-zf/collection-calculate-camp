401c0b00cfde2b5ea16b0fb776ccdfd5145c6201
bfzheng <baofeng_zheng@163.com>
2018-09-04 17:43:57 +0800

    [reduce] modify compute median

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,7 +2,9 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
-
+  for (var obj in collection) {
+    // console.log(obj);
+  }
 }
 
 module.exports = compute_chain_median;
--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -2,6 +2,7 @@
 
 function compute_median(collection) {
   //在这里写入代码
+  collection = collection.sort((a, b) => a > b ? 1 : -1);
   var lowMiddle = Math.floor((collection.length - 1) / 2);
   var highMiddle = Math.ceil((collection.length - 1) / 2);
   var median = (collection[lowMiddle] + collection[highMiddle]) / 2;
