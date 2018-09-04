de82d171fca286baf121c9c8a19c5955233e03d6
bfzheng <baofeng_zheng@163.com>
2018-09-04 16:27:05 +0800

    [collectionOperator] finish get integer interval 2

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -2,6 +2,10 @@
 
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
+  var array = [...Array(Math.abs(number_a - number_b) + 1).keys()].map(value => value + Math.min(number_a, number_b));
+  return number_a < number_b ? array.filter(value => value % 2 === 0) : array.filter(value => value % 2 === 0).reverse();
 }
 
 module.exports = get_integer_interval_2;
+
+
--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,5 +1,27 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  const selector = (arr, val) => {
+    if (val.includes('-')) {
+      var str = val.split('-');
+      arr[str[0]] = str[1];
+      return arr;
+    }
+    if (val in arr) {
+      arr[val]++;
+    } else {
+      arr[val] = 1;
+    }
+    return arr;
+  }
+  var array = collection.reduce(selector, {});
+  var res = [];
+  for (var obj in array) {
+    var temp = new Object();
+    temp.key = obj;
+    temp.count = array[obj];
+    res.push(temp);
+  }
+  return res;
 }
 
 module.exports = count_same_elements;
