1fac64362a75f7d2759e3117382f75d6ab4c8afe
bfzheng <baofeng_zheng@163.com>
2018-09-04 16:33:51 +0800

    [collectionOperator] finish get letter interval

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,6 +2,8 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  var array = [...Array(Math.abs(number_a - number_b) + 1).keys()].map(value => String.fromCharCode(value + Math.min(number_a, number_b) + 'a'.charCodeAt(0) - 1));
+  return number_a < number_b ? array : array.reverse();
 }
 
 module.exports = get_letter_interval;
