04c5466e9a59c72b15e0e743ea4969228eb363d5
bfzheng <baofeng_zheng@163.com>
2018-09-04 10:23:34 +0800

    [superposition operation] finish own elements operation subscript is even even group calculate average

--- practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,11 +1,14 @@
 'use strict';
 var even_group_calculate_average = function (collection) {
-  // var arry = collection.filter(value => (collection.indexOf(value) + 1) % 2 === 0).filter(number => number % 2 === 0);
-  // var res = [];
-  // const sum = (a, b) => (a + b) / 2;
-  // for (var i = 0; Math.pow(10,i) < collection.sort()[collection.length - 1]; i++) {
-  //   res.push(arry.filter(number => number > Math.pow(10, i) && number < Math.pow(10, i + 1)).reduce(sum));
-  // }
-  // return res;
-};
+  var arry = collection.filter(value => (collection.indexOf(value) + 1) % 2 === 0).filter(number => number % 2 === 0);
+  var res = [];
+  const sum = (a, b) => (a + b);
+  for (var i = 0; Math.pow(10, i) < arry.sort()[arry.length - 1]; i++) {
+    var temp = arry.filter(number => (number > Math.pow(10, i) - 1 && number < Math.pow(10, i + 1)));
+    if (temp.length > 0) {
+      res.push(temp.reduce(sum) / temp.length);
+    }
+  }
+  return res;
+}
 module.exports = even_group_calculate_average;
