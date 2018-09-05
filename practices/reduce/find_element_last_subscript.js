2017193313d84f81eaedc2c0bc1ecd0ebfc62e0e
bfzheng <baofeng_zheng@163.com>
2018-09-05 10:18:23 +0800

    [reduce] modify find element last subscript to use lastIndexOf function

--- practices/reduce/find_element_last_subscript.js
+++ practices/reduce/find_element_last_subscript.js
@@ -2,13 +2,7 @@
 
 function calculate_elements_sum(collection, element) {
   //在这里写入代码
-  var indexs = [];
-  var idx = collection.indexOf(element);
-  while (idx != -1) {
-    indexs.push(idx);
-    idx = collection.indexOf(element, idx + 1);
-  }
-  return indexs[indexs.length - 1];
+  return collection.lastIndexOf(element);
 }
 
 module.exports = calculate_elements_sum;
