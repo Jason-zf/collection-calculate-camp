0c77fa7d1a13d01729d8211388d55d8bfc72da17
bfzheng <baofeng_zheng@163.com>
2018-09-05 13:55:47 +0800

    [section 2] modify practice 3

--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -1,21 +1,9 @@
+var selector = require('../../../common/count_same_elements_selector');
+
 function count_same_elements(collection) {
-  //在这里写入代码
-  const selector = (arr, val) => {
-    var c = Number.isNaN(parseInt(val.replace(/[^0-9]/g, ''))) === true ? 1 : parseInt(val.replace(/[^0-9]/g, ''));
-    var k = val.match(/^[A-Za-z]*/g)[0];
-    var flag = false;
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i].name === k) {
-        arr[i].summary += c;
-        flag = true;
-      }
-    }
-    if (flag === false) {
-      arr.push({'name': k, 'summary': c});
-    }
-    return arr;
-  }
-  return collection.reduce(selector, []);
+  return collection.reduce(selector, []).map(value => {
+    return {'name': value.key, 'summary': value.count}
+  });
 }
 
 module.exports = count_same_elements;
